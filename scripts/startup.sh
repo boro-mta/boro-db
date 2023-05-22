d35a23f5a1c0aa5b36782b4b5968ba19d6f2f128
Alon Mordechai <58881462+alon-m@users.noreply.github.com>
2023-05-22 10:32:35 +0300

    remove empty lines in startup,sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 set -e
-
 # Define variables
 export MSSQL_PID=Developer
 export MSSQL_SA_PASSWORD=Passw0rd1
 export MSSQL_TCP_PORT=1433
 export DATABASE_NAME=BoroMainDB
 export MSSQL_TLS_CERT_VERIFY_MODE=0
-
 # Start SQL Server
 /opt/mssql/bin/sqlservr &
-
 # Wait for SQL Server to start
 echo "Waiting for SQL Server to start..."
 sleep 30
-
 # Run SQL scripts
 echo "Creating database schema and inserting data..."
-
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $MSSQL_SA_PASSWORD -i /scripts/dbinit.sql -b &
-
 # Keep SQL Server running in the foreground
 echo "SQL Server is running..."
 while : ; do
